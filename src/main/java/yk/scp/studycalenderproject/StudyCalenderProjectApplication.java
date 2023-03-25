c815c89f2275236c49582f5af82ac34cf0fc4a39
nkkeem <nkkeem@gmail.com>
2023-03-25 14:26:54 +0900

    Initial commit

---
+++ src/main/java/yk/scp/studycalenderproject/StudyCalenderProjectApplication.java
@@ -0,0 +1,13 @@
+package yk.scp.studycalenderproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCalenderProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCalenderProjectApplication.class, args);
+    }
+
+}
