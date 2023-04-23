2959ae3d77ebe242a442c0900c5eb75944a73c28
jdh96 <jdh961224@gmail.com>
2023-04-23 18:01:32 +0900

    리액트 라우터 예시 올림

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,19 +4,21 @@ import Main from "./Sample/Main";
 import CalendarPage from "./Sample/CalendarPage";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NotFound from "./Sample/error/NotFound";
+import Footer from "./Sample/Footer";
 
 function App() {
   const ref = useRef(null);
 
   return (
     <BrowserRouter ref={ref}>
-            <Header />
+        <Header />
             <Routes>
                 <Route path="/" element={<Main />}></Route>
                 <Route path="/calendar" element={<CalendarPage />}></Route>
                 {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
                 <Route path="*" element={<NotFound />}></Route>
             </Routes>
+        <Footer />
     </BrowserRouter>
   );
 }
