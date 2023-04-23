eb0d403d041a5370441091f78152ac401c9d05df
jdh96 <jdh961224@gmail.com>
2023-04-23 22:52:57 +0900

    링크 추가

--- src/main/frontend/src/Sample/Main.js
+++ src/main/frontend/src/Sample/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function Main(props) {
     return (
         <>
             <h1>Main</h1>
+            <Link to="/calendar">Calendar로 이동!</Link>
         </>
     );
 }
