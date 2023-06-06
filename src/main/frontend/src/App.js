8edae21b4dd5b07ea1be6b9923b7b20b92d63bff
jdh96 <jdh961224@gmail.com>
2023-06-06 13:40:37 +0900

    Main 홈페이지 수정 (header, footer 는 메인페이지에서만 보이게)

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,30 +1,24 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Header from './pages/Header';
-import Main from './pages/Main';
 import CalendarPage from './pages/calendar/CalendarPage';
 import Signup from './pages/user/Signup';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotFound from './pages/error/NotFound';
-import Footer from './pages/Footer';
-import Todo from './pages/todo/toDo';
-import toDoInsert from "./pages/todo/toDoInsert";
-import toDoTemplate from "./pages/todo/toDoTemplate";
+import Todo from './pages/todo/Todo';
+import Home from "./pages/Home";
 
 function App() {
   const ref = useRef(null);
 
   return (
     <BrowserRouter ref={ref}>
-      <Header />
       <Routes>
-        <Route path="/" element={<Main />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/calendar" element={<CalendarPage />}></Route>
         <Route path="/user/signup" element={<Signup />}></Route>
         <Route path="/board" element={<Todo />}></Route>
         {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/main/frontend/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Main from "./Main";
+import Footer from "./Footer";
+
+
+function Home(props) {
+    return (
+        <div>
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
+
+export default Home;
