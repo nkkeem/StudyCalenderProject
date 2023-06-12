2ec93ad43de206ba3c04aa143083e2e1acfc53fb
jdh96 <jdh961224@gmail.com>
2023-06-12 20:58:26 +0900

    todo check 구현

--- src/main/frontend/src/pages/todo/TodoItem.jsx
+++ src/main/frontend/src/pages/todo/TodoItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import CheckBoxOutlineBlankOutlinedIcon from '@mui/icons-material/CheckBoxOutlineBlankOutlined';
 import RemoveCircleOutlineRoundedIcon from '@mui/icons-material/RemoveCircleOutlineRounded';
@@ -6,73 +6,81 @@ import CheckBoxOutlinedIcon from '@mui/icons-material/CheckBoxOutlined';
 import cn from 'classnames';
 
 const Div = styled.div`
-  display: flex;
-  height: 50px;
-  &:hover {
-    background: #eeeeee;
-  }
+    display: flex;
+    height: 50px;
+    &:hover {
+        background: #eeeeee;
+    }
 `;
 
 const Content = styled.div`
-  display: flex !important;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  width: 100%;
-  padding: 1rem;
-  &.checked{
-    color: #adb5bd;
-    text-decoration: line-through;
-  }
+    display: flex !important;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    padding: 1rem;
+    &.checked {
+        color: #adb5bd;
+        text-decoration: line-through;
+    }
 `;
 
 const Button = styled.button`
-  display: flex;
-  flex: 1;
-  svg {
-    font-size: 2rem;
-  }
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  background: none;
-  outline: none;
-  border: none;
-
-  &.deleteBox {
+    display: flex;
+    flex: 1;
     svg {
-      color: #ff6b6b;
-      &:hover {
-        color: red;
-      }
+        font-size: 2rem;
     }
-  }
-  &.checkBox {
-    svg {
-      &:hover {
-        color: #a9a9a9;
-      }
-    }
-  }
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    background: none;
+    outline: none;
+    border: none;
 
+    &.deleteBox {
+        svg {
+            color: #ff6b6b;
+            &:hover {
+                color: red;
+            }
+        }
+    }
+    &.checkBox {
+        svg {
+            &:hover {
+                color: #a9a9a9;
+            }
+        }
+    }
 `;
 
 function TodoItem({ todo }) {
-  const { content, checked } = todo;
+    const { content, checked } = todo;
+    const [toggle, setToggle] = useState(checked);
 
-  return (
-    <Div className='TodoItem'>
-      <Button className={cn('checkBox', { checked })}>
-        {checked ? <CheckBoxOutlinedIcon /> : <CheckBoxOutlineBlankOutlinedIcon />}
-      </Button>
-      <Content className={cn({ checked })}>
-        {content}
-      </Content>
-      <Button className='deleteBox'>
-        <RemoveCircleOutlineRoundedIcon />
-      </Button>
-    </Div>
-  );
+    return (
+        <Div className="TodoItem">
+            <Button
+                className={cn('checkBox', { checked })}
+                onClick={() => {
+                    console.log(`checked! ${toggle}`);
+                    setToggle(!toggle);
+                }}
+            >
+                {toggle ? (
+                    <CheckBoxOutlinedIcon />
+                ) : (
+                    <CheckBoxOutlineBlankOutlinedIcon />
+                )}
+            </Button>
+            <Content className={cn({ checked })}>{content}</Content>
+            <Button className="deleteBox">
+                <RemoveCircleOutlineRoundedIcon />
+            </Button>
+        </Div>
+    );
 }
 
 export default TodoItem;
